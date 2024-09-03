096d9ac522abb331e9e25d14ed8b56960ea0960e
semeniak97mf <n.semeniak@moneyfactory.io>
2024-09-03 14:33:00 +0300

    Optimize check_ssl.sh

--- docker/nginx-container/check_ssl.sh
+++ docker/nginx-container/check_ssl.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+CERT_DIR="/etc/nginx/ssl/live/polkadot-explorer.atleta.network"
+FULLCHAIN_PEM="$CERT_DIR/fullchain.pem"
+PRIVKEY_PEM="$CERT_DIR/privkey.pem"
+MD5SUM_FILE="/tmp/md5sum"
+
 while true; do
   sleep 3600
-  if [ "$(md5sum /etc/nginx/ssl/live/polkadot-explorer.atleta.network/fullchain.pem \
-    /etc/nginx/ssl/live/polkadot-explorer.atleta.network/privkey.pem | md5sum)" != \
-    "$(cat /tmp/md5sum || echo '')" ]; then
+  if [ "$(md5sum "$FULLCHAIN_PEM" "$PRIVKEY_PEM" | md5sum)" != "$(cat "$MD5SUM_FILE" || echo '')" ]; then
     nginx -s reload
-    md5sum /etc/nginx/ssl/live/polkadot-explorer.atleta.network/fullchain.pem \
-    /etc/nginx/ssl/live/polkadot-explorer.atleta.network/privkey.pem | md5sum > /tmp/md5sum
+    md5sum "$FULLCHAIN_PEM" "$PRIVKEY_PEM" | md5sum > "$MD5SUM_FILE"
   fi
 done
